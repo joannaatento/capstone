ac5bfcecec519a90fc197ab425f048b71a5298f6
joannaatento <06802182@dwc-legazpi.edu>
2023-05-05 21:46:34 +0800

    errors fixed

---
+++ admin/adminlogin.php
@@ -0,0 +1,77 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is already logged in, if yes then redirect him to welcome page
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: admindashboard.php");
+    exit;
+}
+// Include config file
+require_once "../connection.php";
+require_once "function/adminloginf.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>Signin Simple</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
+<!-- --------------style css --------------->
+<link rel="stylesheet" href="styleuser.css">
+
+</head>
+<body>
+<div class="login-form">
+    <form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <div class="text-center">
+            <a href="index.html" aria-label="Space">
+                <img class="mb-3" src="assets/image/logo.png" alt="Logo" width="60" height="60">
+            </a>
+          </div>
+        <div class="text-center mb-4">
+            <h1 class="h3 mb-0">Please sign in</h1>
+            <p>Signin to manage your account.</p>
+        </div>
+     
+        <div class="js-form-message mb-3" <?= (!empty($username_err)) ? 'has-error' : ''; ?>">
+            <div class="js-focus-state input-group form">
+              <div class="input-group-prepend form__prepend">
+                <span class="input-group-text form__text">
+                  <i class="fa fa-user form__text-inner"></i>
+                </span>
+              </div>
+              <input type="text" class="form-control form__input" name="username" placeholder="Username" aria-label="Username" data-msg="Please enter a valid username." data-error-class="u-has-error" data-success-class="u-has-success" value="<?= $username; ?>">
+            </div>
+            <span class="help-block"><?= $username_err; ?></span>
+        </div>
+		<div class="form-group"<?= (!empty($password_err)) ? 'has-error' : ''; ?>">
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <span class="input-group-text">
+                        <i class="fa fa-lock"></i>
+                    </span>                    
+                </div>
+                <input type="password" class="form-control" name="password" placeholder="Password">				
+            </div>
+            <span class="help-block"><?= $password_err; ?></span>
+
+        </div>        
+        <div class="form-group mb-3">
+            <button type="submit" class="btn btn-primary login-btn btn-block">Signin</button>
+        </div>
+        
+        
+        <p class="small text-center text-muted mb-0">All rights reserved. © Space. 2020 soengsouy.com.</p>
+    </form>
+</div>
+
+</body>
+</html>
--- admin/function/adminlogoutf.php
+++ admin/function/adminlogoutf.php
@@ -5,10 +5,11 @@ session_start();
 // Unset all of the session variables
 $_SESSION = array();
  
-// Destroy the session.
+// Destroy the session
 session_destroy();
  
 // Redirect to login page
-header("location: ../loginuser.php");
+header("../adminlogin.php");
 exit;
+
 ?>
